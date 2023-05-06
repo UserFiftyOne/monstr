378368fdff910c08893f4d3f1a717839550737ef
User51 <130700129+UserFiftyOne@users.noreply.github.com>
2023-05-06 15:00:56 -0400

    Initial upload

---
+++ monster_vars.js
@@ -0,0 +1,29 @@
+monster_morale = [ "Fanatic", "Emboldened", "Press on", "Sue for Peace (keeping weapons)", "Surrender", "Strategic disengage", "Flee with abandon (players get a free shot)"]
+
+monster_intelligence = ["Plantlike - contact, proximity", "Bestial - hunger, fear, territoriality", "Low - take simple orders", "Average - human level", "Advanced - smarter than the average bear"]
+
+monster_loyalty = ["Never abandon a post", "Hold until the last possible minute", "Hold while it looks good", "Get out while still ahead", "Get out at first oppotunity", "Take any better offer", "I don't get paid for this s***"]
+
+monster_aggressiveness = ["hesitant to engage", "looking for easy marks", "highly aggressive"]
+
+monster_tactic = ["charge in recklessly", "coordinate as a pack", "try to set up an ambush", "pick off players one at a time", "move around using the terrain", "start with a display or threaten"]
+
+monster_fight_condition = ["on sight", "if the players enter its territory", "it outnumbers the players", "it has surprise", "it is with young or vulnerable",  "it is surprised", "it is cornered", "if players have a certain smell", "if players are wearing or displaying a certain color"]
+
+monster_flight_condition = ["if the players' party contains a cleric", "if the players' party contains a fighter", "if the players' party has a certain smell", "if the players' party has a large creature", "if the players' party makes a loud, sustained noise", "if the players' party has a wizarding type", "if the players' party outnumbers the monsters", "if the players' party charges", "if the players' party has open fire", "if the monster is surprised", "if the players' party is wearing/displaying a certain color"]
+
+monster_fixation = ["looks the biggest", "looks like a cleric", "is the first in line or closest", "is the last in line or farthest away", "looks like a magic slinging type", "is the smallest", "is the strongest", "is the weakest", "is wearing or dislaying a certain color", "looks like they have ranged weapons", "smells a certain way"]
+
+monster_morale_check_pos = ["A player goes down", "Monster magic is successful", "Monster prayer is heard", "Individual player takes 1/2 HP in one blow", "Individual player reduced to half HP", "monster Bard sings", "More than half players go down"]
+monster_morale_check_neg = ["Monster leader is killed/wounded", "Individual monster takes 1/2 HP in one blow", "Individual monster reduced to half HP", "Player magic is successful", "Player prayer is heard", "Player Bard sings", "More than half monsters go down"]
+
+monster_goals = ["capture as many opponents as possible", "delay for reinforcements", "hold this position to the death", "kill or devour all enemies", "prevent opponents from escape", "make a strategic withdrawal"]
+
+monsters_out_doing_unint = ["hunting", "lounging", "defending", "guarding", "wandering", "scouting"]
+monsters_out_doing_int = ["patrolling for intruders", "hunting for basic needs", "raiding the unwary", "looking for treasure", "looking for victims", "guarding a post", "returning to camp/home/den", "traveling to meet up with others", "transporting prisoners", "setting traps", "burying something", "making camp", "striking camp", "preparing food", "eating", "carrying stuff", "on their iPads"]
+
+monster_party_comps = [
+	["mature specimen", "young specimen", "seedling"],
+	["adult hunter", "adult fighter", "adult non-combatant", "juvenile combatant", "juvenile non-combatant", "senior non-combatant", "hoary old combatant"],
+	["scout", "bandit", "hunter", "cleric", "pilgrim", "exile", "wizard", "monk", "pilgrim", "fugitive", "guard"]
+]
